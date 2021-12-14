b425ef10e677bfb000fcacf5477e3a321cb7ba22
gabriel-labitcoinera <gabriel@labitcoinera.com>
2021-12-14 10:10:42 -0600

    Create models
    Create first models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Transaccion(models.Model):
+  id = models.BigAutoField(primary_key=True)
+  amount_hnl = models.FloatField()
+  amount_btc = models.FloatField()
+  wallet_address = models.CharField(max_length=40)
+  btc_hnl_change = models.FloatField()
+  transaction_id_todopago = models.CharField(max_length=100)
+  fecha_hora = models.DateTimeField(auto_now_add=True)
+  estado = models.ForeignKey('Estado')
+
+class Estado(models.Model):
+  id = models.BigAutoField(primary_key=True)
+  name = models.CharField(max_length=50)
