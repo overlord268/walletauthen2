68dd94a83fcb1da29f065549ec08921e31935870
gabriel-labitcoinera <gabriel@labitcoinera.com>
2021-12-16 12:06:32 -0600

    Depuración de código inútil
    Se eliminaron algunas llamadas a scripts y stylesheets que estaban de más.
    Se corrigieron algunos errores con el mapa y otros con el texto.

--- core/static/core/js/custom.js
+++ core/static/core/js/custom.js
@@ -7,35 +7,3 @@ function getYear() {
 
 getYear();
 
-// client section owl carousel
-$(".client_owl-carousel").owlCarousel({
-    loop: true,
-    margin: 20,
-    dots: false,
-    nav: true,
-    navText: [],
-    autoplay: true,
-    autoplayHoverPause: true,
-    navText: [
-        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
-    responsive: {
-        0: {
-            items: 1
-        },
-        768: {
-            items: 2
-        }
-    }
-});
-
-/** google_map js **/
-
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(14.10089,-87.1843813),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
