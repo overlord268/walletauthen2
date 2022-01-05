7eb142498cf33f41e24aa92eb3da73d059048e96
gabriel-labitcoinera <gabriel@labitcoinera.com>
2022-01-05 11:44:03 -0600

    Nueva conexión con electrum

--- core/functions.py
+++ core/functions.py
@@ -147,21 +147,18 @@ def postTodoPagoPayDirect(token, lempiras, tarjetaNumero, tarjetaNombre, tarjeta
 
 def postElectrum(destination, amount, tokenID, transactionID, externalReference):
   try:
-    user = env('USER_ELECTRUM')
-    password = env('PASSWORD_ELECTRUM')
-    host = '127.0.0.1'
-    port = '7777'
+    host = env('HOST_ELECTRUM')
     bodyPassword = env('PASSWORD_ELECTRUM_WALLET')
-    url = 'http://' + user + ':' + password + '@' + host + ':' + port + '/'
+    public_key = env('PUBLIC_KEY_ELECTRUM')
+    url = 'https://' + host + '/'
     
     headers = CaseInsensitiveDict()
-    headers["Authorization"] = "Basic {}".format(env('PASSWORD_ELECTRUM'))
     headers["Content-Type"] = "application/json"
     
     data = '{"jsonrpc":"2.0","id":"curltext","method":"payto","params":{"destination":"' + destination + '", "amount":"' + str(
       amount) + '", "password":"' + bodyPassword + '"}}'
     
-    result = requests.post(url, headers=headers, data=data)
+    result = requests.post(url, headers=headers, data=data, verify=public_key)
     res = result.json()
     print("postElectrum_res: ", res)
     if "error" in res:
